adc4e5c4abcc061ec3b72973d62bf03a003bf4c7
Panthera Onza <127850374+pantheraonza@users.noreply.github.com>
2026-05-31 19:13:25 +0200

    Add files via upload

---
+++ sh01_v2/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /etc/passwd | grep -v "#" | sed -n '2~2p' | cut -d ':' -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | paste -sd ',' | sed 's/,/, /g' | sed 's/$/.$/' | tr -d '\n'
